bdd3e35f2d622f7f10713308e5692a394a0e79e0
gomezja <jgomez42gb@gmail.com>
2019-07-10 16:59:18 -0700

    added card and deck classes

---
+++ GameLauncher.py
@@ -0,0 +1,41 @@
+import random
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return "{} of {}".format(self.rank, self.suit)
+
+class Deck:
+    def __init__(self):
+        self.deck = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+
+    def __str__(self):
+        curr_deck = "Blackjack Deck\n=============="
+
+        for card in self.deck:
+            curr_deck += "\n" + card.__str__()
+
+        return curr_deck
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+    
+    def deal(self):
+        pass
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+
+# print the contents of the deck
+test_deck = Deck()
+test_deck.shuffle()
+
+print(test_deck)
