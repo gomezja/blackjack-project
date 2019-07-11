6b4a72ba1850e066af862a3dfc8b4f07dd72be1b
gomezja <jgomez42gb@gmail.com>
2019-07-11 16:29:38 -0700

    added hand and chip classes

--- GameLauncher.py
+++ GameLauncher.py
@@ -1,5 +1,6 @@
 import random
 
+#====================Classes====================#
 class Card:
     def __init__(self, suit, rank):
         self.suit = suit
@@ -28,14 +29,88 @@ class Deck:
         random.shuffle(self.deck)
     
     def deal(self):
-        pass
+        return self.deck.pop()
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+    
+    def add_card(self, card):
+        self.cards.append(card)
+
+        card_value = values[card.rank]
+        self.value += card_value
+
+        if card_value == 11:
+            self.aces += 1
+    
+    def adjust_for_ace(self):
+        if self.aces > 0 and self.value > 21:
+            self.value -= 10
+
+class Chips:
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+    
+    def win_bet(self):
+        self.total += self.bet
+    
+    def lose_bet(self):
+        self.total -= self.bet
+
+#====================Functions====================#
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet = int(input("Current amount: {}\nEnter your bet amount: ".format(chips.total)))
+        except:
+            print("Not a number!")
+        else:
+            if chips.bet > chips.total:
+                print("invalid funds")
+            else:
+                break
+
+def hit(deck, hand):
+    hand.add_card(deck.deal())
+
+def hit_or_stand(deck, hand):
+    global playing
+    choice = ""
+
+    while not choice.upper() == 'H' and not choice.upper() == 'S':
+        print("\n====================")
+        choice = input("Hit or stay (H/S)? ")
+        print("\n====================")
+
+    if choice.upper() == 'H':
+        hand.add_card(deck.deal())
+    else:
+        playing = False
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 
-# print the contents of the deck
-test_deck = Deck()
-test_deck.shuffle()
+while True:
+    deck = Deck()
+    deck.shuffle()
 
-print(test_deck)
+    player_chips = Chips()
+    take_bet(player_chips)
+
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+
+    playing = True
+
+    while playing:
+        pass
